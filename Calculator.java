bb2d5076c0be347ed71cc11300114f8d7ff60a77
Sigmaflo2 <kjschocolate@gmail.com>
2018-11-23 19:44:18 +0900

    main, minus

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,15 @@
 public class Calculator(){
+  public static void main(String[] args){
+    int a = 4;
+    int b = 2;
+    System.out.println(sum(a,b));
+  }
+
   public int sum(int a, int b){
     return a + b;
+  }
+
+  public int minus(int a, int b){
+    return a - b;
   }
 }
